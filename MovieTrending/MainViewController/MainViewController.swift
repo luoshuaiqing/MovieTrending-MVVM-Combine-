d17f0c5967fca6d471b698eb7b314889be7ae448
Shuaiqing Luo <luoshuaiqing>
2023-11-12 07:21:27 -0800

    update

---
+++ MovieTrending/MainViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  MovieTrending
+//
+//  Created by Shuaiqing Luo on 11/11/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+        self.title = "Main View"
+    }
+    
+}
