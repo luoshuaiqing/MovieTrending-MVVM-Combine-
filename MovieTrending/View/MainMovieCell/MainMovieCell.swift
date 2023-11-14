cb4242e9b07bcc6b4798ee71a9609da495a6ebf0
Shuaiqing Luo <luoshuaiqing>
2023-11-14 08:34:03 -0800

    update

---
+++ MovieTrending/View/MainMovieCell/MainMovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainMovieCell.swift
+//  MovieTrending
+//
+//  Created by Shuaiqing Luo on 11/14/23.
+//
+
+import UIKit
+
+class MainMovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
